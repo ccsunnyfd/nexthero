3f7bb6cb8a5a01b4c82c3d5be67f8c58295b1753
ccsunnyfd <chzhyfd@126.com>
2019-07-13 20:39:11 +0800

    implement static page of me tab's header part

--- vue-source/pages/me/me.vue
+++ vue-source/pages/me/me.vue
@@ -1,6 +1,20 @@
 <template>
-	<view class="page">
-		我
+	<view class="page page-fill">
+		<view class="header">
+			<image src="../../static/icos/108x108.png" class="face"></image>
+			<view class="info-wrapper">
+				<view class="nickname">
+					NEXT 学院
+				</view>
+				<view class="nav-info">ID: 123abc456xyz</view>
+
+			</view>
+
+			<view class="set-wrapper">
+				<image src="../../static/icos/settings.png" class="settings"></image>
+			</view>
+
+		</view>
 	</view>
 </template>
 
@@ -8,15 +22,15 @@
 	export default {
 		data() {
 			return {
-				
+
 			}
 		},
 		methods: {
-			
+
 		}
 	}
 </script>
 
 <style>
-
+	@import url("me.css");
 </style>
